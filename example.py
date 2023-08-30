d196c8b44854577302f7ceea9034e8c52d946cb3
Stuart Layton <stuart.layton@sevenrooms.com>
2023-08-30 11:44:30 -0400

    wip

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 from dbconfig import DbConfig
-from simplesql.sql import connect
+from simplesql.sql import sql_connect
 
 class Data(BaseModel):
     id: int
@@ -17,12 +17,30 @@ creds = DbConfig(
     log_queries=True
 )
 
+sql_connect(creds).query("TRUNCATE data RESTART IDENTITY").commit()
 
-SQL = connect(creds)
-result = SQL.query("SELECT * FROM data where id=%(id)s").bind({"id": 2}).get_single(Data)
+sql_connect(creds).query("INSERT INTO data (key, value) values (%(key)s, %(value)s) RETURNING *").bind({'key':'k1', 'value':1}).get(Data)
+data = sql_connect(creds).query("SELECT * FROM data where id=%(id)s").bind({"id": 1}).get(Data)
+print(data)
+
+sql_connect(creds).query("INSERT INTO data (key, value) values (%(key)s, %(value)s) RETURNING *").bind({'key':'k2', 'value':2}).get(Data)
 
 id_tuple = ((1,2,),)
-SQL = connect(creds) #Creds is an instance of models. dbconfig
-result = SQL.query("SELECT * FROM data where id in %s").bind(id_tuple).get_list(Data)
-print(result)
+data = sql_connect(creds).query("SELECT * FROM data where id in %s").bind(id_tuple).get_list(Data)
+print(data)
+
+
+# Select by ID
+# SQL = sql_connect(creds)
+# result =
+# print(result)
+#
+# # Select by ID in list of ids
+# id_tuple = ((1,2,),)
+# SQL = sql_connect(creds) #Creds is an instance of models. dbconfig
+# result = SQL.query("SELECT * FROM data where id in %s").bind(id_tuple).get_list(Data)
+# print(result)
+
+# Insert new row into database
+
 
--- simplesql/sql.py
+++ simplesql/sql.py
@@ -1,3 +1,4 @@
+from enum import IntEnum
 from typing import Type
 
 import psycopg2
@@ -7,37 +8,76 @@ from pydantic import BaseModel
 
 from models.dbconfig import DbConfig
 
+class QueryType(IntEnum):
+    undefined = 0
+    select = 1
+    insert = 2
+    delete = 3
+    update = 4
+    truncate = 5
 
 class SQL:
+
+    _COMMIT_TYPES = {QueryType.delete, QueryType.insert, QueryType.update}
     def __init__(self, connection, verbose: bool = False):
         self.connection = connection
         self.verbose = verbose
         self.query_string: str = ""
         self.args: tuple = tuple()
+        self.query_type = QueryType.select
 
     def query(self, query: str):
         self.query_string = query
+        if self.query_string.startswith("SELECT"):
+            self.query_type = QueryType.select
+        elif self.query_string.startswith("INSERT INTO"):
+            self.query_type = QueryType.insert
+        elif self.query_string.startswith("DELETE FROM"):
+            self.query_string = QueryType.delete
+        elif self.query_string.startswith("UPDATE"):
+            self.query_type = QueryType.update
+
         return self
 
     def bind(self, args):
         self.args = args
         return self
 
-    def get_single(self, model_type: Type[BaseModel]) -> BaseModel:
+    def commit(self):
         with self.connection.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute(self.query_string, self.args)
+            self.connection.commit()
+
+    def get(self, model_type: Type[BaseModel]) -> BaseModel:
+        with self.connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(self.query_string, self.args)
+            if self.query_type in self._COMMIT_TYPES:
+                self.connection.commit()
             result = cursor.fetchone()
             return model_type.model_validate(result)
 
     def get_list(self, model_type: Type[BaseModel]) -> list[BaseModel]:
-
         with self.connection.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute(self.query_string, self.args)
+            if self.query_type in self._COMMIT_TYPES:
+                self.connection.commit()
             results = cursor.fetchall()
             return [model_type.model_validate(row) for row in results]
 
+    def get_int(self) -> int:
+        with self.connection.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(self.query_string, self.args)
+            if self.query_type in self._COMMIT_TYPES:
+                self.connection.commit()
+            return cursor.fetchone()
 
-def connect(credentials: DbConfig) -> SQL:
+
+
+
+def sql_connect(credentials: DbConfig) -> SQL:
     conn_str = "dbname='{database}' user='{username}' host='{hostname}' password='{password}'".format_map(credentials.model_dump())
     connection = psycopg2.connect(conn_str)
     return SQL(connection, verbose=credentials.log_queries)
+
+
+
