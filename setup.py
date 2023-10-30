5738b577888a8e8bdebca1a1a13d694dfed20691
Stuart Layton <stuart.layton@sevenrooms.com>
2023-10-30 14:42:22 -0400

    rename to connect-postgres-py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='simplesql',
+    name='connect-postgres-py',
     version='1.0',
-    url='https://github.com/slayton/simple-sql',
+    url='https://github.com/sevenrooms/connect-postgres-py',
     author='slayton',
-    author_email='stuart@stuartlayton.com',
+    author_email='stuart.layton@sevenrooms.com',
     packages=find_packages(),
     install_requires=[
         'psycopg2',
